505000b6d51dac44a5c714952ea54adaae403b3a
DwikiChristian <133040158+DwikiChristian@users.noreply.github.com>
2024-05-29 22:45:13 +0800

    Update swiper.js

--- src/swiper.js
+++ src/swiper.js
@@ -1,42 +1,42 @@
 
-// var swiper = new Swiper(".mySwiper", {
-//   slidesPerView: 3,
-//   spaceBetween: 30,
-//   centerSlide: 'true',
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  centerSlide: 'true',
 
   
-//   breakpoints:{
-//     0: {
-//       slidesPerView :1,
-//     },
-//     520: {
-//       slidesPerView :2,
-//     },
-//     950: {
-//       slidesPerView :3,
-//     }
+  breakpoints:{
+    0: {
+      slidesPerView :1,
+    },
+    520: {
+      slidesPerView :2,
+    },
+    950: {
+      slidesPerView :3,
+    }
    
 
-//   }
-// });
+  }
+});
 
-import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs'
-var swiper = new Swiper(".mySwiper", {
-    spaceBetween: 30,
-    effect: "fade",
-    loop : "true",
-    autoplay: {
-        delay: 2500,
-        disableOnInteraction: false,
-      },
+// import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs'
+// var swiper = new Swiper(".mySwiper", {
+//     spaceBetween: 30,
+//     effect: "fade",
+//     loop : "true",
+//     autoplay: {
+//         delay: 2500,
+//         disableOnInteraction: false,
+//       },
 
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-  });
+//     navigation: {
+//       nextEl: ".swiper-button-next",
+//       prevEl: ".swiper-button-prev",
+//     },
+//     pagination: {
+//       el: ".swiper-pagination",
+//       clickable: true,
+//     },
+//   });
 
